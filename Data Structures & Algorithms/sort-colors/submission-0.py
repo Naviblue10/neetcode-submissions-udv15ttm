8db8ef75226983bcd67d15dac24c65d35b58cd2b
Naviblue10 <150413939+Naviblue10@users.noreply.github.com>
2026-05-28 19:37:40 +0530

    Add: sort-colors - submission-0

---
+++ Data Structures & Algorithms/sort-colors/submission-0.py
@@ -0,0 +1,32 @@
+class Solution:
+    def sortColors(self, nums: List[int]) -> None:
+        i=0
+        max=0
+        flag=False
+        temp_flag=True
+        prev_flag=False
+        while not flag:
+            if i>0:
+                if nums[i-1]>=max:
+                    max=nums[i-1]
+                    temp_flag=True
+                else:
+                    temp_flag=False
+                temp_flag=bool(temp_flag and prev_flag)
+                prev_flag=temp_flag
+            
+            
+            if i<len(nums)-1 and nums[i]>nums[i+1]:
+                nums[i],nums[i+1]=nums[i+1],nums[i]
+            
+            elif i==len(nums)-1: 
+                if temp_flag:
+                    flag=True
+                i=0
+                prev_flag=True
+                temp_flag=True    
+                max=0
+                continue
+            i+=1
+        return nums
+        
