134f2320e89ccd7d1b0f66c82a7e1f56829f5451
Naviblue10 <150413939+Naviblue10@users.noreply.github.com>
2026-09-02 12:03:06 +0530

    Add: rotate-array - submission-1

---
+++ Data Structures & Algorithms/rotate-array/submission-1.py
@@ -0,0 +1,13 @@
+class Solution:
+    def rotate(self, nums: List[int], k: int) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        count=0
+        while count<k:
+            temp=nums[-1]
+            nums.pop()
+            nums.insert(0,temp)
+            count+=1
+        
+
